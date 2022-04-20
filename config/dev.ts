8361114589fc3bc9a1ab864ba8cc0543b4178651
thanaphoom <me@thanaphoom.dev>
2022-04-21 00:11:57 +0700

    update config load env

--- config/dev.ts
+++ config/dev.ts
@@ -1,4 +1,4 @@
 
 
-export const host: string = process.env.api || "http://localhost:5560";
+export const host: string = process.env.NEXT_PUBLIC_API || "http://localhost:5560";
 // export const host: string = "http://192.168.88.158:5560";
